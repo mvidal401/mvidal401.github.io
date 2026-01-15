bf4d98fde5de9b7b3658921ce37d78270486b31f
mvidal401 <mvidal401@iespompeufabra.cat>
2026-01-15 22:04:51 +0100

    Update questions.js

--- selectivitat/questions.js
+++ selectivitat/questions.js
@@ -137,55 +137,7 @@ dibuixar el diagrama de portes lògiques.
             <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
         images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
     },
-    {
-        type: "questions", // Tipus de pregunta
-        category: "materials",
-        text: `Una família que anualment generava 525 kg d’envasos, en un any ha aconseguit reduir aquesta quantitat un 60 %. Quina reducció en emissions de GEH ha obtingut respecte a l’any anterior, tenint en compte que el factor d’emissió dels envasos és de 120,09 g CO2eq/kg residu?`,
-        options: [
-            { text: "25,22 kg CO2eq", value: "a" },
-            { text: "37,83 kg CO2eq", value: "b" },
-            { text: "63,05 kg CO2eq", value: "c" },
-            { text: "88,27 kg CO2eq", value: "d" }
-        ],
-        correctAnswer: "b",
-        steps: `
-            \\[
-            \\text{Quantitat inicial d’envasos} = 525 \\, \\text{kg}
-            \\]
-            \\[
-            \\text{Percentatge de reducció} = 60\\%
-            \\]
-            \\[
-            \\text{Quantitat reduïda} = 525 \\, \\text{kg} \\times 0{,}60 = 315 \\, \\text{kg}
-            \\]
-            \\[
-            \\text{Quantitat de residu restant} = 525 \\, \\text{kg} - 315 \\, \\text{kg} = 210 \\, \\text{kg}
-            \\]
-            \\[
-            \\text{Emissions inicials} = 525 \, \\text{kg} \\times 120{,}09 \, \\text{g CO2eq/kg}
-            \\]
-            \\[
-            \\text{Emissions inicials} = 63047,25 \, \\text{g CO2eq}
-            \\]
-            \\[
-            \\text{Emissions inicials} = 63{,}05 \, \\text{kg CO2eq}
-            \\]
-            \\[
-            \\text{Emissions després de la reducció} = 210 \, \\text{kg} \\times 120{,}09 \, \\text{g CO2eq/kg}
-            \\]
-            \\[
-            \\text{Emissions després de la reducció} = 25218{,}9 \, \\text{g CO2eq}
-            \\]
-            \\[
-            \\text{Emissions després de la reducció} = 25{,}22 \, \\text{kg CO2eq}
-            \\]
-            \\[
-            \\text{Reducció en emissions} = 63{,}05 \, \\text{kg CO2eq} - 25{,}22 \, \\text{kg CO2eq} = 37{,}83 \, \\text{kg CO2eq}
-            \\]
-            <br><br>
-            <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
-        images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
-    },
+ 
      {
         type: "questions", // Tipus de pregunta
         category: "motors",
@@ -223,9 +175,209 @@ dibuixar el diagrama de portes lògiques.
             \\eta = \\frac{2{,}7588 \\times 10^6}{3{,}24 \\times 10^6} \\times 100 = 85{,}15 \\, \\%
             \\]
             <br><br>
+        
+    },
+      {
+        type: "questions", // Tipus de pregunta
+        category: "motors",
+        text: `Es proposen dues opcions de funicular per a anar entre dos punts d’una muntanya a velocitat constant. L’opció A és el recorregut més curt possible, però amb el pendent més acusat. L’opció B recorre més distància per tal de rebaixar el pendent de la via. En totes dues opcions, els funiculars van a la mateixa velocitat i transporten la mateixa quantitat de persones. Es pot afirmar que l’energia total necessària per a completar el recorregut, sense considerar les pèrdues energètiques,`,
+        options: [
+            { text: "és la mateixa.", value: "a" },
+            { text: "és més gran en l’opció A.", value: "b" },
+            { text: "és més gran en l’opció B.", value: "c" },
+            { text: "depèn de l’acceleració màxima del funicular.", value: "d" }
+        ],
+        correctAnswer: "d",
+        steps: `
+            \\[
+            m = 11 \\, \\text{L} \\times 1000 \\, \\text{g/L} = 11000 \\, \\text{g}
+            \\]
+            \\[
+            \\Delta T = 70 \\, ^\\circ \\text{C} - 10 \\, ^\\circ \\text{C} = 60 \\, ^\\circ \\text{C}
+            \\]
+            \\[
+            Q = m \\times c_e \\times \\Delta T
+            \\]
+            \\[
+            Q = 11000 \\, \\text{g} \\times 4{,}18 \\, \\frac{\\text{J}}{\\text{g} \\cdot ^\\circ \\text{C}} \\times 60 \\, ^\\circ \\text{C}
+            \\]
+            \\[
+            Q = 11000 \\times 4{,}18 \\times 60 = 2{,}7588 \\times 10^6 \\, \\text{J}
+            \\]
+            \\[
+            W = 0{,}9 \\, \\text{kWh} \\times 3600000 \\, \\text{J/kWh} = 3240000 \\, \\text{J}
+            \\]
+            \\[
+            \\eta = \\frac{Q}{W} \\times 100
+            \\]
+            \\[
+            \\eta = \\frac{2{,}7588 \\times 10^6}{3{,}24 \\times 10^6} \\times 100 = 85{,}15 \\, \\%
+            \\]
+            <br><br>
+        
+    },
+         {
+        type: "questions", // Tipus de pregunta
+        category: "energia",
+        text: `Alimentem una resistència de valor R = 17 Ω amb una font de tensió alterna sinusoidal de valor eficaç U = 230 V i de freqüència f = 50 Hz. Quin és el valor màxim de corrent que hi circula?`,
+        options: [
+            { text: "13,52 A", value: "a" },
+            { text: "19,13 A", value: "b" },
+            { text: "23,42 A", value: "c" },
+            { text: "27,13 A", value: "d" }
+        ],
+        correctAnswer: "d",
+        steps: `
+            \\[
+            m = 11 \\, \\text{L} \\times 1000 \\, \\text{g/L} = 11000 \\, \\text{g}
+            \\]
+            \\[
+            \\Delta T = 70 \\, ^\\circ \\text{C} - 10 \\, ^\\circ \\text{C} = 60 \\, ^\\circ \\text{C}
+            \\]
+            \\[
+            Q = m \\times c_e \\times \\Delta T
+            \\]
+            \\[
+            Q = 11000 \\, \\text{g} \\times 4{,}18 \\, \\frac{\\text{J}}{\\text{g} \\cdot ^\\circ \\text{C}} \\times 60 \\, ^\\circ \\text{C}
+            \\]
+            \\[
+            Q = 11000 \\times 4{,}18 \\times 60 = 2{,}7588 \\times 10^6 \\, \\text{J}
+            \\]
+            \\[
+            W = 0{,}9 \\, \\text{kWh} \\times 3600000 \\, \\text{J/kWh} = 3240000 \\, \\text{J}
+            \\]
+            \\[
+            \\eta = \\frac{Q}{W} \\times 100
+            \\]
+            \\[
+            \\eta = \\frac{2{,}7588 \\times 10^6}{3{,}24 \\times 10^6} \\times 100 = 85{,}15 \\, \\%
+            \\]
+            <br><br>
+            <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
+        images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
+    },
+
+                  {
+        type: "questions", // Tipus de pregunta
+        category: "energia",
+        text: `En un assaig de tracció s’obtenen les corbes tensió-deformació que mostra la figura. Quin dels dos acers té un límit elàstic més alt?`,
+        options: [
+            { text: "L’acer A.", value: "a" },
+            { text: "L’acer B.", value: "b" },
+            { text: "Ambdós tenen el mateix límit elàstic.", value: "c" },
+            { text: "No es pot determinar amb la informació donada.", value: "d" }
+        ],
+        correctAnswer: "d",
+        steps: `
+            \\[
+            m = 11 \\, \\text{L} \\times 1000 \\, \\text{g/L} = 11000 \\, \\text{g}
+            \\]
+            \\[
+            \\Delta T = 70 \\, ^\\circ \\text{C} - 10 \\, ^\\circ \\text{C} = 60 \\, ^\\circ \\text{C}
+            \\]
+            \\[
+            Q = m \\times c_e \\times \\Delta T
+            \\]
+            \\[
+            Q = 11000 \\, \\text{g} \\times 4{,}18 \\, \\frac{\\text{J}}{\\text{g} \\cdot ^\\circ \\text{C}} \\times 60 \\, ^\\circ \\text{C}
+            \\]
+            \\[
+            Q = 11000 \\times 4{,}18 \\times 60 = 2{,}7588 \\times 10^6 \\, \\text{J}
+            \\]
+            \\[
+            W = 0{,}9 \\, \\text{kWh} \\times 3600000 \\, \\text{J/kWh} = 3240000 \\, \\text{J}
+            \\]
+            \\[
+            \\eta = \\frac{Q}{W} \\times 100
+            \\]
+            \\[
+            \\eta = \\frac{2{,}7588 \\times 10^6}{3{,}24 \\times 10^6} \\times 100 = 85{,}15 \\, \\%
+            \\]
+            <br><br>
+            <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
+        images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
+    },
+
+                  {
+        type: "questions", // Tipus de pregunta
+        category: "energia",
+        text: `Volem refredar un bloc de coure de 40 kg des de 250 °C fins a 25 °C en una hora. La calor específica del coure és ce = 0,385 kJ/(kg °C). Quanta energia cal extreure del sistema?`,
+        options: [
+            { text: "346,5 W s", value: "a" },
+            { text: "962,5 J", value: "b" },
+            { text: "346,5 W h", value: "c" },
+            { text: "3 465 kJ", value: "d" }
+        ],
+        correctAnswer: "d",
+        steps: `
+            \\[
+            m = 11 \\, \\text{L} \\times 1000 \\, \\text{g/L} = 11000 \\, \\text{g}
+            \\]
+            \\[
+            \\Delta T = 70 \\, ^\\circ \\text{C} - 10 \\, ^\\circ \\text{C} = 60 \\, ^\\circ \\text{C}
+            \\]
+            \\[
+            Q = m \\times c_e \\times \\Delta T
+            \\]
+            \\[
+            Q = 11000 \\, \\text{g} \\times 4{,}18 \\, \\frac{\\text{J}}{\\text{g} \\cdot ^\\circ \\text{C}} \\times 60 \\, ^\\circ \\text{C}
+            \\]
+            \\[
+            Q = 11000 \\times 4{,}18 \\times 60 = 2{,}7588 \\times 10^6 \\, \\text{J}
+            \\]
+            \\[
+            W = 0{,}9 \\, \\text{kWh} \\times 3600000 \\, \\text{J/kWh} = 3240000 \\, \\text{J}
+            \\]
+            \\[
+            \\eta = \\frac{Q}{W} \\times 100
+            \\]
+            \\[
+            \\eta = \\frac{2{,}7588 \\times 10^6}{3{,}24 \\times 10^6} \\times 100 = 85{,}15 \\, \\%
+            \\]
+            <br><br>
+            <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
+        images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
+    },
+
+                  {
+        type: "questions", // Tipus de pregunta
+        category: "energia",
+        text: `Alimentem una resistència de valor R = 17 Ω amb una font de tensió alterna sinusoidal de valor eficaç U = 230 V i de freqüència f = 50 Hz. Quin és el valor màxim de corrent que hi circula?`,
+        options: [
+            { text: "13,52 A", value: "a" },
+            { text: "19,13 A", value: "b" },
+            { text: "23,42 A", value: "c" },
+            { text: "27,13 A", value: "d" }
+        ],
+        correctAnswer: "d",
+        steps: `
+            \\[
+            m = 11 \\, \\text{L} \\times 1000 \\, \\text{g/L} = 11000 \\, \\text{g}
+            \\]
+            \\[
+            \\Delta T = 70 \\, ^\\circ \\text{C} - 10 \\, ^\\circ \\text{C} = 60 \\, ^\\circ \\text{C}
+            \\]
+            \\[
+            Q = m \\times c_e \\times \\Delta T
+            \\]
+            \\[
+            Q = 11000 \\, \\text{g} \\times 4{,}18 \\, \\frac{\\text{J}}{\\text{g} \\cdot ^\\circ \\text{C}} \\times 60 \\, ^\\circ \\text{C}
+            \\]
+            \\[
+            Q = 11000 \\times 4{,}18 \\times 60 = 2{,}7588 \\times 10^6 \\, \\text{J}
+            \\]
+            \\[
+            W = 0{,}9 \\, \\text{kWh} \\times 3600000 \\, \\text{J/kWh} = 3240000 \\, \\text{J}
+            \\]
+            \\[
+            \\eta = \\frac{Q}{W} \\times 100
+            \\]
+            \\[
+            \\eta = \\frac{2{,}7588 \\times 10^6}{3{,}24 \\times 10^6} \\times 100 = 85{,}15 \\, \\%
+            \\]
+            <br><br>
             <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
         images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
     }
-    
 ];
 
